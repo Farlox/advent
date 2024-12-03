a15743a6882be1bad5403cbbd191d553667ca30f
Farlox <5080040+Farlox@users.noreply.github.com>
2024-12-02 22:49:19 -0800

    2024: day 3 solved

--- src/2024/03.ts
+++ src/2024/03.ts
@@ -3,29 +3,57 @@ import path from "path";
 
 const main = () => {
   const filePath = path.resolve(__dirname, "03.txt");
-  const lines = fs
-    .readFileSync(filePath, { encoding: "utf8" })
-    .replaceAll("\r", "")
-    .split("\n")
-    .filter((s) => s.length > 0); // ending newline
+  const text = fs.readFileSync(filePath, { encoding: "utf8" });
 
-  console.time("parse");
-  const foos = lines.map(parseLine);
-  console.timeEnd("parse");
+  const regex = /mul\((?<a>\d{1,3}),(?<b>\d{1,3})\)/gm;
 
-  console.log(foos);
+  const p1 = part1(text, regex);
+  console.log(`p1: ${p1}`);
+  // 167090022
+  //  93175512
+
+  const splits = text.split(`do()`);
+  console.log(splits.length);
+
+  let sum = 0;
+
+  let go = true;
+  for (let i = 0; i < splits.length; i++) {
+    const dosplit = splits[i].split(`don't()`);
+    sum += part1(dosplit[0], regex);
+  }
+
+  // for (let i = 0; i < splits.length; i += 2) {
+  //   console.log(`part ${i}`);
+  //   sum += part1(splits[i], regex);
+  // }
+
+  // for (let i = 1; i < splits.length; i += 2) {
+  //   let dosplit = splits[i].split("do()", 2);
+
+  //   // console.log(splits[i].indexOf("do()"));
+
+  //   console.log(`group ${i} split into ${dosplit.length} parts`);
+  //   if (dosplit.length > 1) {
+  //     // console.log(dosplit[1]);
+  //     sum += part1(dosplit[1], regex);
+  //   }
+  // }
+
+  console.log(sum);
 };
 
-interface Foo {
-  line: string;
-}
+const part1 = (text: string, regex: RegExp): number => {
+  let sum: number = 0;
+  let m: RegExpExecArray;
+  while ((m = regex.exec(text)) !== null) {
+    sum += parseInt(m.groups.a) * parseInt(m.groups.b);
+  }
 
-const parseLine = (line: string): Foo => {
-  return {
-    line,
-  };
+  return sum;
 };
 
 console.time("main");
+console.log("> start");
 main();
 console.timeEnd("main");
