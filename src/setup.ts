c83c6155eba0f16292b83c9e0eac161020fbd84f
Farlox <5080040+Farlox@users.noreply.github.com>
2024-12-08 20:57:51 -0800

    updates to template for sample input

--- src/setup.ts
+++ src/setup.ts
@@ -4,63 +4,58 @@ import path from "path";
 import minimist from "minimist";
 
 const main = async () => {
-  const argv = minimist(process.argv.slice(2));
-  console.log(argv);
+    const argv = minimist(process.argv.slice(2));
+    console.log(argv);
 
-  if (argv._.length !== 1) {
-    console.log("From a year directory (e.g. /src/2024/)");
-    console.log("usage: tsx setup.ts 3");
-    return;
-  }
+    if (argv._.length !== 1) {
+        console.log("From a year directory (e.g. /src/2024/)");
+        console.log("usage: tsx setup.ts 3");
+        return;
+    }
 
-  const n: number = argv._[0];
+    const n: number = argv._[0];
 
-  copyTemplate(n);
+    copyTemplate(n);
 
-  fetchInput(n);
-
-  // TODO: download input file
-  // curl https://adventofcode.com/2024/day/#/input --header "Cookie: $(cat .token)" > 0#.txt
+    fetchInput(n);
 };
 
 const copyTemplate = (n: number) => {
-  const templatePath = path.resolve(__dirname, "template.ts");
-  const destPath = path.resolve(process.cwd(), `${n}.ts`.padStart(5, "0"));
+    const templatePath = path.resolve(__dirname, "template.ts");
+    const destPath = path.resolve(process.cwd(), `${n}.ts`.padStart(5, "0"));
 
-  const template = fs.readFileSync(templatePath, { encoding: "utf8" });
+    const template = fs.readFileSync(templatePath, { encoding: "utf8" });
 
-  const parsed = template.replace("##", `${n}`.padStart(2, "0"));
+    const parsed = template.replace("##", `${n}`.padStart(2, "0"));
 
-  console.log(`using ${templatePath} to create ${destPath}`);
-  fs.writeFileSync(destPath, parsed, { encoding: "utf8" });
+    console.log(`using ${templatePath} to create ${destPath}`);
+    fs.writeFileSync(destPath, parsed, { encoding: "utf8" });
 };
 
 const fetchInput = async (n: number) => {
-  const tokenPath = path.resolve(process.cwd(), ".token");
-  const token = fs.readFileSync(tokenPath, { encoding: "utf8" });
+    const tokenPath = path.resolve(process.cwd(), ".token");
+    const token = fs.readFileSync(tokenPath, { encoding: "utf8" });
 
-  const headers = {
-    Cookie: `${token}`,
-  };
+    const headers = {
+        Cookie: `${token}`,
+    };
 
-  const url = `https://adventofcode.com/2024/day/${n}/input`;
+    const url = `https://adventofcode.com/2024/day/${n}/input`;
 
-  console.log(`attempting to fetch ${url} with headers: ${token}`);
+    console.log(`attempting to fetch ${url} with headers: ${token}`);
 
-  const resp = await fetch(url, { headers });
-  if (!resp.ok) {
-    console.error(
-      `error fetching input file: ${resp.status} ${resp.statusText}`
-    );
-    console.error(resp);
-    return;
-  }
+    const resp = await fetch(url, { headers });
+    if (!resp.ok) {
+        console.error(`error fetching input file: ${resp.status} ${resp.statusText}`);
+        console.error(resp);
+        return;
+    }
 
-  const destPath = path.resolve(process.cwd(), `${n}.txt`.padStart(6, "0"));
-  const inputs = await resp.text();
+    const destPath = path.resolve(process.cwd(), `${n}.txt`.padStart(6, "0"));
+    const inputs = await resp.text();
 
-  // TODO: remove ending newline?
-  fs.writeFileSync(destPath, inputs, "utf8");
+    // TODO: remove ending newline?
+    fs.writeFileSync(destPath, inputs, "utf8");
 };
 
 main();
--- src/template.ts
+++ src/template.ts
@@ -2,28 +2,31 @@ import fs from "fs";
 import path from "path";
 
 const main = () => {
-  const filePath = path.resolve(__dirname, "##.txt");
-  const lines = fs
-    .readFileSync(filePath, { encoding: "utf8" })
-    .replaceAll("\r", "")
-    .split("\n")
-    .filter((s) => s.length > 0); // ending newline
+    const isSample = process.argv.length > 2 && process.argv[2] == "--sample";
+    const fileName = `##${isSample ? ".sample" : ""}.txt`;
 
-  console.time("parse");
-  const foos = lines.map(parseLine);
-  console.timeEnd("parse");
+    const filePath = path.resolve(__dirname, fileName);
+    const lines = fs
+        .readFileSync(filePath, { encoding: "utf8" })
+        .replaceAll("\r", "")
+        .split("\n")
+        .filter((s) => s.length > 0); // ending newline
 
-  console.log(foos);
+    console.time("parse");
+    const foos = lines.map(parseLine);
+    console.timeEnd("parse");
+
+    console.log(foos);
 };
 
 interface Foo {
-  line: string;
+    line: string;
 }
 
 const parseLine = (line: string): Foo => {
-  return {
-    line,
-  };
+    return {
+        line,
+    };
 };
 
 console.time("main");
