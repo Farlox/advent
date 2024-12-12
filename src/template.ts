804221c8fb3f3dd3b90749379dbc1b7b117ff06d
Farlox <5080040+Farlox@users.noreply.github.com>
2024-12-11 22:35:00 -0800

    updating template answer output

--- src/template.ts
+++ src/template.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+import { printMatrix } from "./lib";
 
 const main = () => {
     const isSample = process.argv.length > 2 && process.argv[2] == "--sample";
@@ -11,12 +12,19 @@ const main = () => {
         .replaceAll("\r", "")
         .split("\n")
         .filter((s) => s.length > 0); // ending newline
+    // .map(row => row.split('')); // for character array instead of parsed lines
 
     console.time("parse");
     const foos = lines.map(parseLine);
     console.timeEnd("parse");
 
     console.log(foos);
+
+    let part1 = 0;
+    console.log(`part1: ${part1}`);
+
+    let part2 = 0;
+    console.log(`part2: ${part2}`);
 };
 
 interface Foo {
